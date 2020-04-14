819bc8e3e9b28ecbf915287c6d0970b892187e2b
exa85897 <santiago.otero@claro.com.ar>
2020-04-14 13:15:00 -0300

    v1.0.3

--- test.js
+++ test.js
@@ -1,6 +1,26 @@
 console.log("test");
 
-fetch('https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-31.4215852&lon=-64.496266&zoom=10')
+let urlApi = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-31.4215852&lon=-64.496266&zoom=10';
+
+let h = new Headers();
+h.append('Accept','aplication/json');
+
+let req = new Request(urlApi, {
+	method: 'GET',
+	headers: h,
+	mode: 'cors'
+});
+
+fetch(req)
 .then( res => {
-	console.log('res: ',res);
+	
+	if(res.ok){
+		return res.json();
+	}else{
+		console.log( 'res: error' );
+	}
+
+})
+.then( json => {
+	console.log( json.name );
 });
