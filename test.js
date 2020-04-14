ddd6be8ab6d30f44d44db5bcdf07cd17df57afd2
exa85897 <santiago.otero@claro.com.ar>
2020-04-14 11:34:29 -0300

    v1.0.1

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ console.log("test");
 
 fetch('https://nominatim.openstreetmap.org/reverse?format=json&lat=-31.4215852&lon=-64.496266&zoom=10')
 .then( res => {
-	console.log(res);
+	console.log('res: ',res);
 });
